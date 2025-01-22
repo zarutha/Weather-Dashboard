f930f07fd4eb4f3e788f46c90787f80bb2e1d412
zarutha <wangalex24680@gmail.com>
2025-01-21 17:36:28 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const apiKey = process.env.API_KEY;
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', handleSearch);
+locationInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
+        handleSearch();
+    }
+});
+
+function handleSearch() {
+    const location = locationInput.value;
+    if (location) {
+        locationElement.textContent = '';
+        temperatureElement.textContent = '';
+        descriptionElement.textContent = '';
+        fetchWeather(location);
+    }
+}
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Location not found, try somewhere nearby? :(:', error);
+            locationElement.textContent = 'Location not found, check spelling or try somewhere nearby :(';
+        });
+}
